70ad812f75d9daf53d2091b24c8a2b99b94f881a
Marco Cervellera <marco.cervellera@linksmt.it>
2020-04-08 16:33:17 +0200

    ch5

---
+++ ch5/boolean/run.py
@@ -0,0 +1,4 @@
+#Qualsiasi espressione può essere considerata un boolean se ha una logica
+print(5<6)
+print(4>5)
+#i risultati delle espressioni possono essere solo True o False
---
+++ ch5/tuples/run.py
@@ -0,0 +1,6 @@
+#le tuple sono contenute in parentesi
+x = ("red", "blue")
+#gli elementi delle tuple non possono cambiare
+#una volta che una tupla viene creata non è possibile modificarla
+#a causa di questa mancanza di flessibilità sono di solito preferite le liste
+print(x)
